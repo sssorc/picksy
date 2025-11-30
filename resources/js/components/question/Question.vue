6f12f80a8835a4fa0237ee7fd373f3dd1d7209db
Joseph Cross <joseph.cross@atmosphere.us>
2025-11-29 19:21:48 -0500

    start quetions page

---
+++ resources/js/components/question/Question.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import QuestionAnswer from './QuestionAnswer.vue';
+
+const questionText = ref('question test goes here yes please?');
+const props = defineProps({
+    index: {
+        type: Number,
+        required: true,
+    },
+    question: {
+        type: Object,
+        required: true,
+    },
+});
+
+const answers = ref([
+    {
+        id: 1,
+        answer_text: 'first anwer',
+    },
+    {
+        id: 2,
+        answer_text: 'second answer',
+    },
+]);
+
+async function addAnswer() {
+    // TODO
+}
+</script>
+<template>
+    <div class="flex items-start gap-4 rounded-xl border bg-card px-4 py-4 text-card-foreground shadow-sm">
+        <div class="flex shrink-0 items-center justify-center text-xl font-extrabold">{{ props.index }}.</div>
+        <div class="flex-1">
+            <div class="group">
+                <input type="text" class="w-full border border-transparent px-2 text-xl font-bold group-hover:border-input" :value="questionText" />
+            </div>
+            <div v-if="answers.length > 0" class="mt-2 w-sm space-y-2 pl-1">
+                <QuestionAnswer v-for="answer in answers" :key="answer.id" :answer="answer" />
+                <button v-if="answers.length < 4" type="button" class="flex cursor-pointer items-center justify-center bg-stone-100 px-1" @click="addAnswer">Add Answer</button>
+            </div>
+        </div>
+    </div>
+</template>
--- resources/js/pages/admin/QuestionsPage.vue
+++ resources/js/pages/admin/QuestionsPage.vue
@@ -1,25 +1,24 @@
 <script setup lang="ts">
+import Question from '@/components/question/Question.vue';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import AppLayout from '@/layouts/AppLayout.vue';
 import { Head } from '@inertiajs/vue3';
-
-async function handleSubmit() {
-    console.log('submit');
-}
 </script>
 <template>
     <Head title="Questions" />
 
     <AppLayout>
         <div class="flex h-full flex-1 flex-col gap-4 p-4">
+            <h2 class="text-2xl font-bold">Questions</h2>
+            <div>
+                <Question :index="1" />
+            </div>
             <Card>
                 <CardHeader>
                     <CardTitle>Questions</CardTitle>
                     <CardDescription>Create the questinos and answers for your event.</CardDescription>
                 </CardHeader>
-                <CardContent>
-                    <form @submit.prevent="handleSubmit" class="space-y-6">TODO</form>
-                </CardContent>
+                <CardContent> </CardContent>
             </Card>
         </div>
     </AppLayout>
